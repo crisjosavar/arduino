ef72a47c02409f2fd7f4591051f7628e800b4779
crisjosavar <cristhianvar8@gmail.com>
2024-10-23 16:45:21 -0600

    Update sketch_apr25a.ino

--- sketch_apr25a/sketch_apr25a.ino
+++ sketch_apr25a/sketch_apr25a.ino
@@ -1,35 +1,32 @@
-volatile const int led1 = 13;
-volatile const int led2 = 12;
-volatile const int led3 = 11;
+int tiempo = 1000;               // Variable tiempo 150 milisegundos
+int pin;                        // variable pin
 
-const int chave = 2;
+void setup()
 
-void setup() {
-  pinMode(chave, INPUT);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(chave), leds, HIGH);
+{
+  for(pin = 0; pin <= 4; pin++)    // aqui agregamos el ciclo for, pines a utilizar 0,1,2,3,4
+{
+    pinMode(pin, OUTPUT);          // pines del arduino 0,1,2,3,4 se configuran como salida
+  }
 }
 
-void loop() {
-  digitalWrite(led1,HIGH);
-  digitalWrite(led2,LOW);
-  digitalWrite(led3,LOW);
-  delay(1000);
-  digitalWrite(led1,LOW);
-  digitalWrite(led2,HIGH);
-  digitalWrite(led3,LOW);
-  delay(1000);
-  digitalWrite(led1,LOW);
-  digitalWrite(led2,LOW);
-  digitalWrite(led3,HIGH);
-  delay(1000);
-}
+void loop()                       // Empesamos con el secuencial con leds
 
-void leds() {
- digitalWrite(led1,HIGH);
- digitalWrite(led2,HIGH);
- digitalWrite(led3,HIGH);
- delayMicroseconds(1000000);
+{
+  for(pin = 0; pin <= 4; pin++)   // los leds se encenderan y apagaran de a uno en forma creciente
+{
+      digitalWrite(pin, HIGH);    // los leds se encienden         
+      delay(tiempo);
+      digitalWrite(pin, LOW);     // los leds se apagan
+      delay(tiempo);
+ }
+
+   for(pin = 4; pin >= 0; pin--)    // los leds se encenderan y apagaran de a uno en forma decreciente
+{
+      digitalWrite(pin, HIGH);      // los leds se encienden   
+      delay(tiempo);
+      digitalWrite(pin, LOW);       // los leds se apagan
+      delay(tiempo);
+ }
+
 }
